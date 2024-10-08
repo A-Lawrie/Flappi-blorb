fff4925bd274a65c38f587927e5e00b1d956a58d
JCOCHARLIE <lawrieabuna@gmail.com>
2024-10-08 23:44:32 +0300

    Added code to destroy the prfabs after going past a certain point

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float deadZone = -45;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,10 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+        }
         transform.position = transform.position+(Vector3.left * moveSpeed) * Time.deltaTime;
     }
 }
